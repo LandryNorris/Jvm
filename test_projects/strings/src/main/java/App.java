192605544013e2bd85287622c1707bdbc2ef08a2
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2025-06-19 12:57:59 -0500

    Add println method

--- test_projects/strings/src/main/java/App.java
+++ test_projects/strings/src/main/java/App.java
@@ -1,6 +1,7 @@
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Starting");
         String separator = System.lineSeparator();
         String s = "some text";
         String added = s + " and some other text";
