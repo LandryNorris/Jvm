c640cfcda1bbf3382220a4fb47fb219d1249e035
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2025-07-19 10:33:45 -0500

    Restore stringbuilder test

--- test_projects/strings/src/main/java/App.java
+++ test_projects/strings/src/main/java/App.java
@@ -1,13 +1,16 @@
 
 public class App {
     public static void main(String[] args) {
-        Integer i = 60;
-        String s = i.toString();
-        //System.out.println("Starting");
-        //printNull();
-        //printAppendedText();
-        //printStringFromCharArray();
-        // printIntegers();
+        printNull();
+        printAppendedText();
+        printStringFromCharArray();
+        printIntegers();
+    }
+
+    private static void testArgPassing(int i1, int i2, int i3) {
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
     }
 
     private static void printNull() {
@@ -29,18 +32,16 @@ public class App {
     }
 
     private static void printIntegers() {
-        Integer i = new Integer(243843);
-        System.out.println(i);
-//         Integer zero = Integer.valueOf("0");
-//         Integer one = Integer.valueOf("1");
-//         Integer twelve = Integer.valueOf("12");
-//         Integer fiveHundredFortySix = Integer.valueOf("546");
-//         Integer negativeSixThousandFourHundredTwentyOne = Integer.valueOf("-6421");
-//
-//         System.out.println(zero);
-//         System.out.println(one);
-//         System.out.println(twelve);
-//         System.out.println(fiveHundredFortySix);
-//         System.out.println(negativeSixThousandFourHundredTwentyOne);
+        Integer zero = Integer.valueOf(0);
+        Integer one = Integer.valueOf(1);
+        Integer twelve = Integer.valueOf(12);
+        Integer fiveHundredFortySix = Integer.valueOf(546);
+        Integer negativeSixThousandFourHundredTwentyOne = Integer.valueOf(-6421);
+
+        System.out.println(zero);
+        System.out.println(one);
+        System.out.println(twelve);
+        System.out.println(fiveHundredFortySix);
+        System.out.println(negativeSixThousandFourHundredTwentyOne);
     }
 }
