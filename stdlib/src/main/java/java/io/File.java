1964ef776e0d41dbc4d06308cc2c55b261899429
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2025-08-30 19:37:36 -0500

    set file descriptor

--- stdlib/src/main/java/java/io/File.java
+++ stdlib/src/main/java/java/io/File.java
@@ -17,7 +17,7 @@ public class File {
 
     public File(String pathname) {
         // TODO(Landry): Open file lazily when I have other methods
-        int fd = openFile(pathname);
+        fileDescriptor = openFile(pathname);
     }
 
     private native int openFile(String path);
