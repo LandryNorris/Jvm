be402a662476cd59d6a69f45c3ecae1c6df4aae4
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2026-05-16 10:50:32 -0500

    Create test program

---
+++ test_projects/inheritance/src/main/java/Bar.java
@@ -0,0 +1,12 @@
+public class Bar extends Foo {
+    int barIntField;
+    float barFloatField;
+    String barStringField;
+
+    Bar() {
+        super();
+        barIntField = 5;
+        barFloatField = 68.2f;
+        barStringField = "BAR";
+    }
+}
